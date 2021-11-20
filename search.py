59f87a64150f943b873ba490d6ec5f944888a205
Sidharth Shah <sidharth@fafadiatech.com>
2021-11-20 15:52:34 +0530

    ref #1: minor code cleanup

--- search.py
+++ search.py
@@ -14,9 +14,8 @@ def get_book_title(name):
         f"http://openlibrary.org/search.json?q={name}")
 
     result_dict = result.json()
-    #click.echo(f"Title: {result_dict['docs'][0]['title']}")
-    for i in result_dict['docs']:
-        print("Title: ", i['title'])
+    for current in result_dict['docs']:
+        print("Title: ", current['title'])
 
 
 if __name__ == "__main__":
